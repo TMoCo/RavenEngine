1d69e19ff60422d5fb7054dbdbc67c33f0cade65
prime.zeng <zhunbeifang2@outlook.com>
2021-03-07 06:50:18 +0800

    config files

---
+++ RavenEngine/Editor/premake5.lua
@@ -0,0 +1,98 @@
+
+
+project "RavenEditor"
+	kind "ConsoleApp"
+	language "C++"
+	editandcontinue "Off"
+	debugdir (root_dir.."/gameProject/")
+	
+	files
+	{
+		"**.h",
+		"**.cpp",
+		"Source/**.h",
+		"Source/**.cpp"
+	}
+	
+	includedirs
+	{
+		"../RavenEngine/Source",
+	}
+
+	sysincludedirs
+	{
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glew}",
+		"%{IncludeDir.stb}",
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.Dependencies}",
+		"%{IncludeDir.spdlog}",
+		"%{IncludeDir.cereal}",
+		"%{IncludeDir.Raven}",
+		"%{IncludeDir.OpenFBX}",
+		"%{IncludeDir.glm}",
+	}
+
+	links
+	{
+		"RavenEngine",
+		"imgui",
+		"spdlog",
+	}
+
+	defines
+	{
+		"SPDLOG_COMPILED_LIB"
+	}
+
+	filter { "files:Dependencies/**"}
+		warnings "Off"
+
+	filter 'architecture:x86_64'
+		defines { "RAVEN_SSE"}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		--entrypoint "WinMainCRTStartup"
+		--entrypoint "mainCRTStartup"
+
+		defines
+		{
+
+			"_CRT_SECURE_NO_WARNINGS",
+			"_DISABLE_EXTENDED_ALIGNED_STORAGE",
+			"_SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING",
+		}
+
+		libdirs
+		{
+			--"../RAVEN/Dependencies/libs" 
+		}
+
+		links
+		{
+			"glfw",
+			"OpenGL32",
+		}
+
+		disablewarnings { 4307 }
+
+	filter "configurations:Debug"
+		defines { "LUMOS_DEBUG", "_DEBUG","TRACY_ENABLE","LUMOS_PROFILE", }
+		symbols "On"
+		runtime "Debug"
+		optimize "Off"
+
+	filter "configurations:Release"
+		defines { "LUMOS_RELEASE","TRACY_ENABLE", "LUMOS_PROFILE",}
+		optimize "Speed"
+		symbols "On"
+		runtime "Release"
+
+	filter "configurations:Production"
+		defines "LUMOS_PRODUCTION"
+		symbols "Off"
+		optimize "Full"
+		runtime "Release"
---
+++ RavenEngine/Game/premake5.lua
@@ -0,0 +1,98 @@
+
+project "Game"
+	--CONSOLEAPP  or WindowedApp
+	kind "ConsoleApp"
+	language "C++"
+	debugdir (root_dir.."/gameProject/")
+
+	files
+	{
+		"**.h",
+		"**.cpp",
+		"Scenes/**.h",
+		"Scenes/**.cpp"
+	}
+	
+
+	sysincludedirs
+	{
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glew}",
+		"%{IncludeDir.stb}",
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.Dependencies}",
+		"%{IncludeDir.spdlog}",
+		"%{IncludeDir.cereal}",
+		"%{IncludeDir.Raven}",
+		"%{IncludeDir.OpenFBX}",
+		"%{IncludeDir.glm}",
+	}
+
+	includedirs
+	{
+		"../RavenEngine/Raven/Source",
+	}
+
+	links
+	{
+		"RavenEngine",
+		"imgui",
+		"spdlog",
+	}
+
+	defines
+	{
+		"SPDLOG_COMPILED_LIB"
+	}
+
+	filter { "files:Dependencies/**"}
+		warnings "Off"
+
+	filter 'architecture:x86_64'
+		defines { "RAVEN_SSE"}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		--entrypoint "WinMainCRTStartup"
+		--entrypoint "mainCRTStartup"
+		defines
+		{
+			"_CRT_SECURE_NO_WARNINGS",
+			"_DISABLE_EXTENDED_ALIGNED_STORAGE",
+			"_SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING",
+		}
+
+		libdirs
+		{
+			--"../RAVEN/Dependencies/libs" 
+		}
+
+		links
+		{
+			"glfw",
+			"OpenGL32",
+		}
+
+		disablewarnings { 4307 }
+	
+
+	filter "configurations:Debug"
+		defines { "RAVEN_DEBUG", "_DEBUG","TRACY_ENABLE","RAVEN_PROFILE", }
+		symbols "On"
+		runtime "Debug"
+		optimize "Off"
+		
+
+	filter "configurations:Release"
+		defines { "RAVEN_RELEASE","TRACY_ENABLE", "RAVEN_PROFILE",}
+		optimize "Speed"
+		symbols "On"
+		runtime "Release"
+
+	filter "configurations:Production"
+		defines "RAVEN_PRODUCTION"
+		symbols "Off"
+		optimize "Full"
+		runtime "Release"

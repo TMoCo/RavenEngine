97a5786fba37d8bc573944538fe5fc7f774bea44
Sulagna Sinha <sc17s2s@leeds.ac.uk>
2021-03-21 20:31:33 +0000

    Write heights to file

--- RavenEngine/Raven/Source/ProceduralGenerator/TerrainGeneration.cpp
+++ RavenEngine/Raven/Source/ProceduralGenerator/TerrainGeneration.cpp
@@ -1,5 +1,6 @@
 #include "TerrainGeneration.h"
 #include <iostream>
+#include <fstream>
 
 namespace Raven {
 
@@ -25,18 +26,21 @@ namespace Raven {
 
 		const int width = 128;
 		const int height = 128;
+		const int octaves = 4;
 		float a = 0.1;
 		float b = 1.0;
 
 		// buffer to store noise data
-		GLubyte* data = new GLubyte[width * height * 4];
-		//double data[width * height * 4];
+		//GLubyte* data = new GLubyte[width * height * octaves];
+		float data[width * height * octaves];
 		
 		float xFactor = 1.0f / (width - 1);
 		float yFactor = 1.0f / (height - 1);
 
-		for (int row = 0; row < height; row++) {
-			for (int col = 0; col < width; col++) {
+		for (int row = 0; row < height; row++)
+		{
+			for (int col = 0; col < width; col++)
+			{
 				float x = xFactor * col;
 				float y = yFactor * row;
 				float sum = 0.0f;
@@ -44,32 +48,54 @@ namespace Raven {
 				float scale = b;
 
 				// compute the sum for each octave
-				for (int oct = 0; oct < 4; oct++) {
+				for (int oct = 0; oct < octaves; oct++)
+				{
 					glm::vec2 p(x * freq, y * freq);
 					float val = glm::perlin(p) / scale;
 					sum += val;
 					float result = (sum + 1.0f) / 2.0f;
 					// store in texture buffer
-					data[((row * width + col) * 4) + oct] = (GLubyte)(result * 255.0f);
-					//data[((row * width + col) * 4) + oct] = result * 255.0f;
-					
+					//data[((row * width + col) * octaves) + oct] = (GLubyte)(result * 255.0f);
+					data[((row * width + col) * octaves) + oct] = result * 255.0f;
+					//LOGI(data[((row * width + col) * octaves) + oct]);
+
 					freq *= 2.0f; // double the frequency
 					scale *= b; // next power of b
 				}
 			}
 		}
 
-		//for (int i = 0; i < width*height*4; i++)
-		//{
-		//	LOGE(data[i]);
-		//}
+		std::ostringstream outFile;
 
-		GLuint texID;
-		glGenTextures(1, &texID);
-		glBindTexture(GL_TEXTURE_2D, texID);
-		glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA8, width, height);
-		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, data);
-		delete [] data;
+		outFile << "heightmap.ppm";
+
+		std::ofstream out(outFile.str().c_str());
+
+		out << "P3 " << width << " " << height << " 255\n";
+
+		for (int row = 0; row < height; row++)
+		{
+			for (int col = 0; col < width; col++)
+			{
+				for (int oct = 0; oct < octaves; oct++)
+				{
+					if (oct == 3)
+					{
+						out << (int)data[((row * width + col) * octaves) + oct] << ' ' << (int)data[((row * width + col) * octaves) + oct] << ' ' << (int)data[((row * width + col) * octaves) + oct] << '\n';
+					}
+										
+				}
+			}
+		}
+
+		out.close();
+
+		//GLuint texID;
+		//glGenTextures(1, &texID);
+		//glBindTexture(GL_TEXTURE_2D, texID);
+		//glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA8, width, height);
+		//glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		//delete [] data;
 		
 	}
 }
